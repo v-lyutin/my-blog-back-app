d75d822b214cc86032808e86bf7aac353af1bcae
Vladislav Lyutin <dyson-sv25@yandex.ru>
2025-10-10 23:56:56 +0200

    feat: add GlobalExceptionHandler

--- src/main/java/com/amit/common/configuration/WebConfiguration.java
+++ src/main/java/com/amit/common/configuration/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.amit.common.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,7 +11,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(
         basePackages = "com.amit",
         useDefaultFilters = false,
-        includeFilters = @ComponentScan.Filter(RestController.class)
+        includeFilters = {
+                @ComponentScan.Filter(RestController.class),
+                @ComponentScan.Filter(ControllerAdvice.class)
+        }
 )
 public class WebConfiguration {
 }
